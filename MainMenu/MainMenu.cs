12201f0cf326b460719e39725704c9eb27b2bebb
Jakub Formanek <xforman2@fi.muni.cz>
2025-07-12 20:12:22 +0200

    feat: finished menu without styles

---
+++ MainMenu/MainMenu.cs
@@ -0,0 +1,36 @@
+using Godot;
+using System;
+
+public partial class MainMenu : Control
+{
+    public override void _Ready()
+    {
+        Button startButton = GetNode<Button>("VBoxContainer/Start");
+        Button optionsButton = GetNode<Button>("VBoxContainer/Options");
+        Button quitButton = GetNode<Button>("VBoxContainer/Quit");
+
+        startButton.Pressed += OnStartPressed;
+        optionsButton.Pressed += OnOptionsPressed;
+        quitButton.Pressed += OnQuitPressed;
+
+    }
+
+    private void OnStartPressed()
+    {
+        GetTree().ChangeSceneToFile("res://game.tscn");
+
+    }
+
+    private void OnQuitPressed()
+    {
+        GetTree().Quit();
+
+    }
+
+    private void OnOptionsPressed()
+    {
+        GetTree().ChangeSceneToFile("res://MainMenu/options.tscn");
+
+    }
+}
+
