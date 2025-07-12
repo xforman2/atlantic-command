369558672a463ff5c1650c42a57245a370ff9760
Jakub Formanek <xforman2@fi.muni.cz>
2025-07-12 10:34:38 +0200

    feat: menu

---
+++ MainMenu.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public partial class MainMenu : Control
+{
+	public override void _Ready()
+	{
+		Button startButton = GetNode<Button>("Start");
+
+		startButton.Pressed += OnStartPressed;
+	}
+
+	private void OnStartPressed()
+	{
+		GD.Print("Start pressed!");
+		GetTree().ChangeSceneToFile("res://game.tscn");
+	}
+}
