81d79400a387b345f2069be929dbe120919113a8
Jakub Formanek <xforman2@fi.muni.cz>
2025-07-17 21:16:13 +0200

    fix: make esc go to main menu

--- UIOverlay/UIOverlay.cs
+++ UIOverlay/UIOverlay.cs
@@ -3,42 +3,42 @@ using Godot;
 
 public partial class UIOverlay : Control
 {
-    private Label woodLabel;
-    private Label coalLabel;
-    private Label ironLabel;
-    private Label copperLabel;
+	private Label woodLabel;
+	private Label coalLabel;
+	private Label ironLabel;
+	private Label copperLabel;
 
-    public override void _Ready()
-    {
-        woodLabel = GetNode<Label>("Wood");
-        coalLabel = GetNode<Label>("Coal");
-        ironLabel = GetNode<Label>("Iron");
-        copperLabel = GetNode<Label>("Copper");
+	public override void _Ready()
+	{
+		woodLabel = GetNode<Label>("Wood");
+		coalLabel = GetNode<Label>("Coal");
+		ironLabel = GetNode<Label>("Iron");
+		copperLabel = GetNode<Label>("Copper");
 
-        ResourceMediator.OnResourceChanged += OnResourceChanged;
-    }
+		ResourceMediator.OnResourceChanged += OnResourceChanged;
+	}
 
-    public override void _ExitTree()
-    {
-        ResourceMediator.OnResourceChanged -= OnResourceChanged;
-    }
+	public override void _ExitTree()
+	{
+		ResourceMediator.OnResourceChanged -= OnResourceChanged;
+	}
 
-    private void OnResourceChanged(object sender, string resourceName, int newAmount)
-    {
-        switch (resourceName)
-        {
-            case nameof(PlayerResourceManager.Wood):
-                woodLabel.Text = $"{newAmount}";
-                break;
-            case nameof(PlayerResourceManager.Coal):
-                coalLabel.Text = $"{newAmount}";
-                break;
-            case nameof(PlayerResourceManager.Iron):
-                ironLabel.Text = $"{newAmount}";
-                break;
-            case nameof(PlayerResourceManager.Copper):
-                copperLabel.Text = $"{newAmount}";
-                break;
-        }
-    }
+	private void OnResourceChanged(object sender, string resourceName, int newAmount)
+	{
+		switch (resourceName)
+		{
+			case nameof(PlayerResourceManager.Wood):
+				woodLabel.Text = $"{newAmount}";
+				break;
+			case nameof(PlayerResourceManager.Coal):
+				coalLabel.Text = $"{newAmount}";
+				break;
+			case nameof(PlayerResourceManager.Iron):
+				ironLabel.Text = $"{newAmount}";
+				break;
+			case nameof(PlayerResourceManager.Copper):
+				copperLabel.Text = $"{newAmount}";
+				break;
+		}
+	}
 }
