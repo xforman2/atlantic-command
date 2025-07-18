03bbe677622f044dbb2e5730a8b2beb3093b7546
Jakub Formanek <xforman2@fi.muni.cz>
2025-07-18 15:45:23 +0200

    fix: make esc go to main menu

--- Autoload/InputManager.cs
+++ Autoload/InputManager.cs
@@ -2,11 +2,12 @@ using Godot;
 
 public partial class InputManager : Node
 {
-    public override void _UnhandledInput(InputEvent @event)
-    {
-        if (@event.IsActionPressed("ui_cancel"))
-        {
-            GetTree().ChangeSceneToFile("res://MainMenu/MainMenu.tscn");
-        }
-    }
+	public override void _UnhandledInput(InputEvent @event)
+	{
+		GD.Print("test");
+		if (@event.IsActionPressed("ui_cancel"))
+		{
+			GetTree().ChangeSceneToFile("res://MainMenu/MainMenu.tscn");
+		}
+	}
 }
