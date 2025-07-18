70ed74eec75eeafb794a885a0c908988d1ada702
Jakub Formanek <xforman2@fi.muni.cz>
2025-07-18 15:51:37 +0200

    fix: remove unused print

--- Autoload/InputManager.cs
+++ Autoload/InputManager.cs
@@ -2,12 +2,11 @@ using Godot;
 
 public partial class InputManager : Node
 {
-	public override void _UnhandledInput(InputEvent @event)
-	{
-		GD.Print("test");
-		if (@event.IsActionPressed("ui_cancel"))
-		{
-			GetTree().ChangeSceneToFile("res://MainMenu/MainMenu.tscn");
-		}
-	}
+    public override void _UnhandledInput(InputEvent @event)
+    {
+        if (@event.IsActionPressed("ui_cancel"))
+        {
+            GetTree().ChangeSceneToFile("res://MainMenu/MainMenu.tscn");
+        }
+    }
 }
