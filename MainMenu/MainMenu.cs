f00b46f5a6ee50ab69ad2383d7a072848c7d00ce
GDamcoK <damian.adamek06@gmail.com>
2025-08-09 21:04:47 +0200

    fix

--- MainMenu/MainMenu.cs
+++ MainMenu/MainMenu.cs
@@ -3,40 +3,40 @@ using System;
 
 public partial class MainMenu : Control
 {
-    public override void _Ready()
-    {
-        Button startButton = GetNode<Button>("VBoxContainer/Start");
-        Button optionsButton = GetNode<Button>("VBoxContainer/Options");
-        Button quitButton = GetNode<Button>("VBoxContainer/Quit");
+	public override void _Ready()
+	{
+		Button startButton = GetNode<Button>("VBoxContainer/Start");
+		Button optionsButton = GetNode<Button>("VBoxContainer/Options");
+		Button quitButton = GetNode<Button>("VBoxContainer/Quit");
 
-        startButton.Pressed += OnStartPressed;
-        optionsButton.Pressed += OnOptionsPressed;
-        quitButton.Pressed += OnQuitPressed;
+		startButton.Pressed += OnStartPressed;
+		optionsButton.Pressed += OnOptionsPressed;
+		quitButton.Pressed += OnQuitPressed;
 
-    }
+	}
 
-    private void OnStartPressed()
-    {
-        var ship = ShipManager.Instance.CurrentShip;
-        if (ship is not null)
-        {
-            ship.EnableCamera();
-        }
+	private void OnStartPressed()
+	{
+		var ship = ShipManager.Instance.CurrentShip;
+		if (ship is not null)
+		{
+			ship.EnableCamera();
+		}
 
-        GetTree().ChangeSceneToFile("res://ShipBuilder/ShipBuilder.tscn");
+		GetTree().ChangeSceneToFile("res://ShipBuilder/ShipBuilder.tscn");
 
 
-    }
+	}
 
-    private void OnQuitPressed()
-    {
-        GetTree().Quit();
+	private void OnQuitPressed()
+	{
+		GetTree().Quit();
 
-    }
+	}
 
-    private void OnOptionsPressed()
-    {
-        GetTree().ChangeSceneToFile("res://MainMenu/Options.tscn");
+	private void OnOptionsPressed()
+	{
+		GetTree().ChangeSceneToFile("res://MainMenu/Options.tscn");
 
-    }
+	}
 }
